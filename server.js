bab97aca99622ab89f51b38736e889f18085eb95
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-05 12:39:55 +0100

    Start adding register

--- server.js
+++ server.js
@@ -2,29 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const connectToDB = require('./db');
-
-const adsRoutes = require('./routes/adverts.routes');
-const authRoutes = require('./routes/auth.routes');
+// start express server
 
 const app = express();
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
 
+// connect to DB
+connectToDB();
+
+// add middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connectToDB();
+// serve static files from React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/api', adsRoutes);
-app.use('/auth', authRoutes);
+// add routes
+app.use('/api', require('./routes/adverts.routes'));
+app.use('/api', require('./routes/users.routes'));
+app.use('/auth', require('./routes/auth.routes'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-//   });
+// at any other link serve React app
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
-});
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
 });
