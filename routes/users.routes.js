92330a64975dd4ac0c89ca6817b3c9a92c1ff683
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-05 13:53:34 +0100

    Add login functionality

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -29,4 +29,27 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    if (
+      login &&
+      typeof login === 'string' &&
+      password &&
+      typeof password === 'string'
+    ) {
+      const user = await User.findOne({ login });
+      if (!user) {
+        res.status(400).send({ message: 'Login or password are incorrect' });
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          res.status(200).send({ message: 'Login successful' });
+        } else {
+          res.status(400).send({ message: 'Login or password are incorrect' });
+        }
+      }
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
