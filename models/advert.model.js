7660469a6013b86a2788497b722a4fde6f70384f
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-09 14:20:37 +0100

    Add additional conditions

--- models/advert.model.js
+++ models/advert.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const advertSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, minlength: 10, maxlength: 50 },
+  description: { type: String, required: true, minlength: 20, maxlength: 1000 },
   date: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
   localization: { type: String, required: true },
-  user: { type: String, require: true, ref: 'User' },
+  user: { type: String, required: true, ref: 'User' },
 });
 
 module.exports = mongoose.model('Advert', advertSchema);
