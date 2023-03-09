65d0bc81f443750592a9bbaf872b8baf9681f51e
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-09 14:13:58 +0100

    Update auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,12 +3,14 @@ const User = require('../models/user.model');
 
 exports.register = async (req, res) => {
   try {
-    const { login, password } = req.body;
+    const { login, password, phoneNumber } = req.body;
     if (
       login &&
       typeof login === 'string' &&
       password &&
-      typeof password === 'string'
+      typeof password === 'string' &&
+      phoneNumber &&
+      typeof phoneNumber === 'string'
     ) {
       const userWithLogin = await User.findOne({ login });
       if (userWithLogin) {
@@ -19,8 +21,9 @@ exports.register = async (req, res) => {
       const user = await User.create({
         login,
         password: await bcrypt.hash(password, 10),
+        phoneNumber,
       });
-      res.status(201).send({ message: 'User created ' + user.login });
+      res.status(201).send({ message: `User created:  ${user.login}` });
     } else {
       res.status(400).send({ message: 'Bad request' });
     }
@@ -41,17 +44,16 @@ exports.login = async (req, res) => {
       const user = await User.findOne({ login });
       if (!user) {
         res.status(400).send({ message: 'Login or password are incorrect' });
+      } else if (bcrypt.compareSync(password, user.password)) {
+        req.session.login = user.login;
+        res.status(200).send({ message: 'Login successful' });
       } else {
-        if (bcrypt.compareSync(password, user.password)) {
-          req.session.login = user.login;
-          res.status(200).send({ message: 'Login successful' });
-        } else {
-          res.status(400).send({ message: 'Login or password are incorrect' });
-        }
+        res.status(400).send({ message: 'Login or password are incorrect' });
       }
-    } else {
-      res.status(400).send({ message: 'Bad request' });
     }
+    // } else {
+    //   res.status(400).send({ message: 'Bad request' });
+    // }
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
@@ -59,4 +61,13 @@ exports.login = async (req, res) => {
 
 exports.getUser = async (req, res) => {
   res.send(`"I'm logged!"`);
+};
+
+exports.logout = async (req, res) => {
+  try {
+    req.session.destroy();
+    res.send('Bye Bye');
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
