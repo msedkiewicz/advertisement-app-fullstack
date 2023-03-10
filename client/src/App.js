5dcbc1feb37c33b63b1c3384973b55e4b8ffb7ed
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-10 11:30:35 +0100

    Add components to site

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import Footer from './components/views/Footer/Footer';
 import Header from './components/views/Header/Header';
+import { fetchData } from './redux/adsRedux';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchData()), [dispatch]);
   return (
     <main>
       <Header />
--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchData } from '../../../redux/adsRedux';
+import Loader from '../../features/Loader/Loader';
+import LineupAds from '../../features/LineupAds/LineupAds';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [pending, setPending] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+    setPending(false);
+  }, [dispatch]);
+
+  console.log(pending);
+  return (
+    <>
+      {pending && <Loader />}
+      {!pending && <LineupAds />}
+    </>
+  );
 };
 
 export default Home;
