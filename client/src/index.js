8edf6eea84f1941d54c292aa8c357fac974594e1
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-10 11:27:26 +0100

    Add bootstrap; code formatting

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import store from './redux/store'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -13,5 +14,5 @@ root.render(
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
