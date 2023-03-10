aaa25e7ade2659bcc9975691f003f30635fba4cd
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-10 11:28:21 +0100

    Add Loader

---
+++ client/src/components/features/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="d-flex justify-content-center mt-5 mb-5">
+      <div className="spinner-border" role="status">
+        <span className="sr-only"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
